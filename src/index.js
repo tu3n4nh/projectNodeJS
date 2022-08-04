92ce2b06be2b5a766e7575b7d9fb14df7d556ec1
AT180204 <buituananh3423@gmail.com>
2022-08-05 03:02:04 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const { engine } = require("express-handlebars");
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'resources/public')));
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('hbs', engine({
+  extname: '.hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+console.log(__dirname);
+// route '/'
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
+
+
