525bcd01661161ba2796ad8006c45a3f20b71d3c
AT180204 <buituananh3423@gmail.com>
2022-08-09 02:06:44 +0700

    deo su dung prettier nua dung Shift+Alt+F

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
-const { engine } = require("express-handlebars");
+const { engine } = require('express-handlebars');
 const port = 3000;
 
 const route = require('./routes/index.js'); // hoặc ./routes thôi vì nó tự nạp file index
@@ -11,18 +11,23 @@ const route = require('./routes/index.js'); // hoặc ./routes thôi vì nó t�
 app.use(express.static(path.join(__dirname, '/public')));
 
 //Create middleware
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 //HTTP logger
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', engine({
-   extname: '.hbs',
-}));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 console.log(__dirname);
@@ -31,10 +36,8 @@ console.log(__dirname);
 //Home, search, contact
 
 // Routes initial
-route(    app);
+route(app);
 
 app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-})
-
-
+    console.log(`Example app listening on port ${port}`);
+});
